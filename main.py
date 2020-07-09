cd3dd9747c56086bb4ef9a3e3a2f8cc874ba8ba6
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-09 17:26:42 +0200

    Add basic script logic

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, world!!!")
+import requests, json
+
+country = "czech_republic"
+url = f"https://top-ghusers.vercel.app/api?c={country}"
+
+responce = requests.get(url)
+resJson = json.loads(responce.text)
+
+for user in resJson['users']:
+    if user['user']['username'] == 'filiptronicek':
+        print("Got to him!")
