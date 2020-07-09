af0026e8b66840a6455509d6ad40f7abab47e991
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-09 22:24:19 +0200

    write changed

--- modify.py
+++ modify.py
@@ -6,9 +6,14 @@ patt = '<a.*class="ranking".*></a>'
 def write(rank):
     with open(filename) as f:
         content = f.readlines()
-    content = [x.strip() for x in content] 
+    content = [x for x in content] 
+    replaced = ""
     for c in content:
         if re.search(patt, c):
-            modify = (c.split(re.split(patt, c)[0])[1]).replace("><",f">{str(rank)}<")
-            print(modify)
-            break
+            splitToModify = (c.split(re.split(patt, c)[0])[1])
+            modify = splitToModify.replace("><",f">{str(rank)}<")
+            replaced += c.split(splitToModify)[0] + modify + c.split(splitToModify)[1]
+        else:
+            replaced += c
+    with open(filename, "w") as f:
+        f.write(replaced)
